67481792e78f137f83fdac463b54fcc312870e41
JAMES SMITH <jomak200025@gmail.com>
2022-02-03 16:22:44 +0000

    Added Shell functions for some policies

--- ParticleEmitter.cpp
+++ ParticleEmitter.cpp
@@ -1,1 +1,16 @@
 #include "ParticleEmitter.h"
+
+void Emission_policies::SphereEmission::Emit()
+{
+	
+}
+
+void Motion_policies::Constant::UpdatePositions()
+{
+	
+}
+
+void Deletion_policies::LifeSpan::DeleteParticles()
+{
+	
+}
--- ParticleEmitter.h
+++ ParticleEmitter.h
@@ -6,71 +6,71 @@ struct Vector3Float
 	float x, y, z;
 };
 
-namespace Emission_Policies
+namespace Emission_policies
 {
 	class ConeEmission				//Emits particles in cone shape 
 	{
 		Vector3Float	m_dir;		//Direction of the cone
 		float			m_maxAngle;	//Maximum angle of emission around the direction
 	protected:
-		Emit();
+		void Emit();
 	};
 	class SphereEmission			//Simply emits randomly in all directions from a point
 	{
 	protected:
-		Emit();
+		void Emit();
 	};
 	class CircleEmission			//Emits particles in random directions on a plan defined by a normal vector
 	{
 		Vector3Float m_normal;		//The normal of the circle can be used to 
 	protected:
-		Emit();
+		void Emit();
 	};
 }
 
-namespace Motion_Policies			//These are used to define how the particles will move after emission
+namespace Motion_policies			//These are used to define how the particles will move after emission
 {
 	class Accelerating
 	{
 		float m_initVel;			//The initial velocity of the particles when emitted
 		float m_acceleration;		//The rate of acceleration for particles
 	protected:
-		UpdatePositions();
+		void UpdatePositions();
 	};
 	class Constant
 	{
-		float m_Vel;				//The velocity of the particles when emitted
+		float m_vel;				//The velocity of the particles when emitted
 	protected:
-		UpdatePositions();
+		void UpdatePositions();
 	};
 	class WithGravity
 	{
 		float m_vel;				//The velocity of emitted particles
 		float m_gravity;			//The strength of gravity for the particles
 	protected:
-		UpdatePositions();
-	}
+		void UpdatePositions();
+	};
 }
 
-namespace Deletion_Policies			//These are used to define how when particles are culled
+namespace Deletion_policies			//These are used to define how when particles are culled
 {
 	class LifeSpan
 	{
 		float m_maxLifeTime;		//This is used to define how long, in seconds, a particle has before being culled
 	protected:
-		DeleteParticles();
+		void DeleteParticles();
 	};
 	class CubeBoundaries
 	{
 		Vector3Float m_bounds;		//Defines how far in each direction a particle can travel before being culled
 	protected:
-		DeleteParticles();
+		void DeleteParticles();
 	};
 	class SphereBoundaries
 	{
 		float m_maxDistance;		//Defines how far a particle can travel from the emitted befor it is culled
 	protected:
-		DeleteParticles();
+		void DeleteParticles();
 	};
 }
 
